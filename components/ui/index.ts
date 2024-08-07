436c3e7f5df2a8838e2a3416f46405d4d694ab99
Sviatoslav Melnychuk <svyatoslav200104@gmail.com>
2024-08-07 14:09:19 +0300

    Add ui components and some basic markup.

---
+++ components/ui/index.ts
@@ -0,0 +1,7 @@
+export { Button } from "./button";
+export { Input } from "./input";
+export { Checkbox } from "./checkbox";
+export { Dialog } from "./dialog";
+export { Select } from "./select";
+export { Skeleton } from "./skeleton";
+export { Slider } from "./slider";
